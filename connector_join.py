641bd7e8a0d0c00e87e17fcbc9f1ff7cac747b1c
doyoung <doyoh1179@gmail.com>
2021-07-26 08:07:04 +0800

    Example - Join Method

---
+++ connector_join.py
@@ -0,0 +1,6 @@
+# Connecting iterable elements using an empty string
+array = ['H', 'E', 'L', 'L','O', ' ', 'W','O','R','L','D']
+connector = ""
+joined_string = connector.join(array)
+
+print(joined_string)
