6c5e3532693d0e65e668774fa9e5f0578b34ae77
doyoung <doyoh1179@gmail.com>
2022-04-27 09:29:37 +0900

    Algorithm Practice - Leetcode

---
+++ leetcode_practice/leet703_kth_largest_element_in_a_stream.py
@@ -0,0 +1,32 @@
+"""
+Design a class to find the kth largest element in a stream. Note that it is
+the kth largest element in the sorted order, not the kth distinct element.
+
+Implement KthLargest class:
+KthLargest(int k, int[] nums) Initializes the object with the integer k and
+the stream of integers nums. int add(int val) Appends the integer val to the
+stream and returns the element representing the kth largest element in the
+stream.
+"""
+from typing import List
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        #min heap with K largest integers
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+        
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap, val)
+        if len(minheap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
