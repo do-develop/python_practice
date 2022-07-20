1eb27e6cad8c32a841e05bef73a6d955d3b76fd7
doyoung <doyoh1179@gmail.com>
2022-07-20 09:12:01 +0900

    Algorithm Practice - Leetcode

---
+++ leetcode_practice/leet212_word_search2.py
@@ -0,0 +1,65 @@
+"""
+Given an m x n board of characters and a list of strings words, return all
+words on the board. Each word must be constructed from letters of sequentially
+adjacent cells, where adjacent cells are horizontally or vertically neighboring.
+The same letter cell may not be used more than once in a word.
+"""
+from typing import List
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end_of_word = False
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end_of_word = True
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        for w in words:
+            root.addWord(w)
+
+        rows, cols = len(board), len(board[0])
+        result, visited = set(), set()
+
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 or
+                r == rows or c == cols or
+                (r, c) in visited or
+                board[r][c] not in node.children):
+                return
+
+            visited.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.end_of_word:
+                result.add(word)
+
+            dfs(r - 1, c, node, word)
+            dfs(r + 1, c, node, word)
+            dfs(r, c - 1, node, word)
+            dfs(r, c + 1, node, word)   
+            visited.remove((r, c))
+                
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, root, "")
+
+        return list(result)
+
+
+# TEST
+board = [["o","a","a","n"],
+         ["e","t","a","e"],
+         ["i","h","k","r"],
+         ["i","f","l","v"]]
+words = ["oath","pea","eat","rain"]
+print(Solution().findWords(board, words))
+# expected output: ["eat","oath"]
