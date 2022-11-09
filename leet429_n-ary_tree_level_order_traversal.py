f7c9e86a5da122f1336708810c923904121e9707
do-develop <doyoh1179@gmail.com>
2022-11-09 18:42:13 +0100

    Algorithm Practice - Leetcode

---
+++ leet429_n-ary_tree_level_order_traversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        if not root:
+            return []
+        q, ans = deque([root]), []
+        
+        while q:
+            level = []
+            for i in range(len(q)):
+                node = q.popleft()
+                for child in node.children:
+                    q.append(child)
+                level += [node.val]
+            ans += [level]
+        return ans
