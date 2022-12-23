f5d6cb4e573c2df13cdca502383667a20f660626
do-develop <83389587+do-develop@users.noreply.github.com>
2022-12-23 22:02:32 +0100

    Algorithm Practice - Leetcode

---
+++ leet538_convert_bst_to_greater_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.cursum = 0
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.right)
+            self.cursum += node.val
+            node.val = self.cursum
+            dfs(node.left)
+        dfs(root)
+        return root
