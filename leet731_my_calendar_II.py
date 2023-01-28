31929052646c6a5f28a1226dd61a8ed0f7e6a1c5
do-develop <83389587+do-develop@users.noreply.github.com>
2023-01-28 17:52:20 +0100

    Algorithm Practice - Leetcode

---
+++ leet731_my_calendar_II.py
@@ -0,0 +1,21 @@
+class MyCalendarTwo:
+
+    def __init__(self):
+        self.calendar = []
+        self.overlaps = []
+
+    def book(self, start: int, end: int) -> bool:
+        for s, e in self.overlaps:
+            if start < e and end > s:
+                return False
+        for s, e in self.calendar:
+            if start < e and end > s:
+                # smaller area is the overlapping zone
+                self.overlaps.append((max(start, s),min(end, e)))
+        self.calendar.append((start, end))
+        return True
+
+
+# Your MyCalendarTwo object will be instantiated and called as such:
+# obj = MyCalendarTwo()
+# param_1 = obj.book(start,end)
