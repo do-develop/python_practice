7c9361950dbc0287dfb1dff5eecafe4da530e9f7
do-develop <83389587+do-develop@users.noreply.github.com>
2023-04-21 11:23:44 +0200

    Algorithm Practice - Leetcode

---
+++ leet1004_max_consecutive_ones_III.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        # sliding window approach
+        l = 0
+        for r in range(len(nums)):
+            k -= (1 - nums[r])
+            if k < 0:
+                k += (1 - nums[l])
+                l += 1
+        return r - l + 1
