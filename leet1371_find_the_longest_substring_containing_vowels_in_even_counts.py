8f22affb8a611de799a1a02025aa399746894c5b
do-develop <83389587+do-develop@users.noreply.github.com>
2023-08-02 16:45:00 +0200

    Algorithm Practice - Leetcode

---
+++ leet1371_find_the_longest_substring_containing_vowels_in_even_counts.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        substrings = self.generate_substring(s)
+        res = 0
+        for substring in substrings:
+            is_all_even = True
+            for ch in 'aeiou':
+                if substring.count(ch) % 2: # not even
+                    is_all_even = False
+                    break
+            if is_all_even:
+                return len(substring)
+        return res
+
+
+    def generate_substring(self, s: str):
+        for window_size in range(len(s), -1, -1):
+            for i in range(len(s) - window_size + 1):
+                yield s[i: i + window_size]
