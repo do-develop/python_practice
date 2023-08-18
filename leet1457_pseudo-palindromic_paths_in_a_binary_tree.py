5c5f5e2901df2b4553155ea50364f23f81dd71b7
do-develop <83389587+do-develop@users.noreply.github.com>
2023-08-18 11:11:12 +0200

    Algorithm Practice - Leetcode

---
+++ leet1457_pseudo-palindromic_paths_in_a_binary_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        def dfs(node, pairs):
+            if not node:
+                return 0
+            if node.val in pairs:
+                pairs.remove(node.val)
+            else:
+                pairs.add(node.val)
+            # path to the leaf node
+            if not node.left and not node.right:
+                # found palindrome then 1 else 0
+                return 1 if len(pairs) <= 1 else 0
+            
+            left = dfs(node.left, set(pairs))
+            right = dfs(node.right, set(pairs))
+            return left + right
+        
+        return dfs(root, set())
