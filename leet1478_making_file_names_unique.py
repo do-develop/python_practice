48e742b2b772ffae9f0022c3fb182d9352c48a01
do-develop <83389587+do-develop@users.noreply.github.com>
2023-08-24 21:26:02 +0200

    Algorithm Practice - Leetcode

---
+++ leet1478_making_file_names_unique.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getFolderNames(self, names: List[str]) -> List[str]:
+        last = {}
+        for name in names:
+            modified = name
+            if name in last:
+                k = last[name]
+                while modified in last:
+                    k += 1
+                    modified =  f'{name}({k})'
+                last[name] = k
+            last[modified] = 0
+        return last.keys()
