4b17175fa236370b4b5002724c8b757d5b85b74a
do-develop <83389587+do-develop@users.noreply.github.com>
2023-12-20 17:11:59 +0900

    Algorithm Practice - Leetcode

---
+++ leet1845_seat_reservation_manager.py
@@ -0,0 +1,15 @@
+class SeatManager:
+# optimize the initialization by using next variable
+    def __init__(self, n: int):
+        self.next = 1
+        self.heap = []
+
+    def reserve(self) -> int:
+        if self.heap and self.heap[0] < self.next:
+            return heapq.heappop(self.heap)
+        
+        self.next += 1
+        return self.next - 1
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.heap, seatNumber)
