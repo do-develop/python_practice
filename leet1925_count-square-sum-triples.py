5ca7676695f8a957e610c48c9ca5ac8de9296580
do-develop <83389587+do-develop@users.noreply.github.com>
2024-01-24 08:23:17 +0100

    Algorithm Practice - Leetcode

---
+++ leet1925_count-square-sum-triples.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        count = 0
+        x = [x for x in range(1, n + 1)]
+        for i in range(1, n):
+            for j in range(1, n):
+                a = x[i]
+                b = x[j]
+                c = (a * a) + (b * b)
+                k = math.sqrt(c)
+                if k in x:
+                    count += 1
+        return count
