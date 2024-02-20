8fde82ac29d890d3535531b8492d6111598e29b9
do-develop <83389587+do-develop@users.noreply.github.com>
2024-02-20 16:25:47 +0100

    Algorithm Practice - Leetcode

---
+++ leet1981_minimize_the_difference_between_target_and_chosen_elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        possible_min = sum(min(row) for row in mat)
+        if possible_min > target:
+            return possible_min - target
+        
+        nums = {0}
+        for row in mat:
+            nums = {x + i for x in row for i in nums if x + i <= 2 * target - possible_min}
+        
+        return min(abs(target - x) for x in nums)
