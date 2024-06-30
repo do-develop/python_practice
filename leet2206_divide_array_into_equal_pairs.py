4975a78a99a371767faacab6bc82f644979ad0c9
do-develop <83389587+do-develop@users.noreply.github.com>
2024-06-30 15:07:51 +0200

    Algorithm Practice - Leetcode

---
+++ leet2206_divide_array_into_equal_pairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        seen = set()
+        for n in nums:
+            if n in seen:
+                seen.remove(n)
+            else:
+                seen.add(n)
+        
+        return len(seen) == 0
