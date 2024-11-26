87746710fa3dd91635fd3bd505d6f915d720c0df
do-develop <83389587+do-develop@users.noreply.github.com>
2024-11-26 21:04:32 +0100

    Algorithm Practice in Golang

---
+++ leet2442_count_number_of_distinct_integers_after_reverse_operations.go
@@ -0,0 +1,17 @@
+func countDistinctIntegers(nums []int) int {
+    numsMap := make(map[int]bool, len(nums) * 2)
+    for i := 0; i < len(nums); i++ {
+        numsMap[nums[i]] = true
+        numsMap[reverse(nums[i])] = true
+    }
+    return len(numsMap)
+}
+
+func reverse(num int) int {
+    rev := 0
+    for num > 0 {
+        rev = (rev * 10) + (num % 10)
+        num /= 10
+    }
+    return rev
+}
