ca6c02ad8a1a8d11586ad91e67a4b1986ba36d0a
do-develop <83389587+do-develop@users.noreply.github.com>
2025-02-25 15:44:34 +0100

    Algorithm Practice in Golang

---
+++ leet2616_minimize_the_maximum_difference_of_pairs.go
@@ -0,0 +1,27 @@
+func minimizeMax(nums []int, p int) int {
+    sort.Ints(nums)
+    left, right := 0, nums[len(nums) - 1] - nums[0]
+
+    for left < right {
+        mid := (left + right) / 2
+        if countPairs(nums, mid, p) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+
+func countPairs(nums[] int, mid int, p int) bool {
+    count := 0
+    for i := 0; i < len(nums) - 1 && count < p; {
+        if nums[i + 1] - nums[i] <= mid {
+            count++
+            i += 2
+        } else {
+            i++
+        }
+    }
+    return count >= p
+}
