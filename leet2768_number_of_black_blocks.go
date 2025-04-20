1ff8c1b3a5b44d17e5925135f4d171f2bd536afd
do-develop <83389587+do-develop@users.noreply.github.com>
2025-04-20 21:45:58 +0200

    Algorithm Practice

---
+++ leet2768_number_of_black_blocks.go
@@ -0,0 +1,23 @@
+func countBlackBlocks(m int, n int, coordinates [][]int) []int64 {
+    cnt := map[int64]int{}
+    dirs := [5]int{0, 0, -1, -1, 0}
+
+    for _, c := range coordinates {
+        x, y := c[0], c[1]
+        for k := 0; k < 4; k++ {
+            i, j := x + dirs[k], y + dirs[k + 1]
+            if i >= 0 && i < m - 1 && j >= 0 && j < n - 1 {
+                cnt[int64(i * n + j)]++
+            }
+        }
+    }
+
+    ans := make([]int64, 5)
+    ans[0] = int64((m - 1) * (n - 1))
+
+    for _, x := range cnt {
+        ans[x]++
+        ans[0]--
+    }
+    return ans
+}
