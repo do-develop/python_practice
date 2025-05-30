5d4c0bb2af3d2602f60fddc46b596cc8518b8ea4
do-develop <83389587+do-develop@users.noreply.github.com>
2025-05-30 18:40:04 +0200

    Algorithm Practice

---
+++ leet2828_check-if-a-string-is-an-acronym-of-words.go
@@ -0,0 +1,12 @@
+func isAcronym(words []string, s string) bool {
+    if len(words) != len(s) {
+        return false
+    }
+
+    for i := 0; i < len(words); i++ {
+        if words[i][0] != s[i] {
+            return false
+        }
+    }
+    return true
+}
