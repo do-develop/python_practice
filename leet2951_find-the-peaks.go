6daab6562b472966e6272e21e5677cbc52442b21
do-develop <83389587+do-develop@users.noreply.github.com>
2025-08-15 15:55:39 +0200

    Algorithm Practice

---
+++ leet2951_find-the-peaks.go
@@ -0,0 +1,10 @@
+func findPeaks(mountain []int) []int {
+    var peaks []int
+
+    for i := 1; i < len(mountain) - 1; i++ {
+        if mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1] {
+            peaks = append(peaks, i)
+        }
+    }
+    return peaks
+}
