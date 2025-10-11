7f63b9967761d646b2bb5348511ddad9ee54b142
do-develop <83389587+do-develop@users.noreply.github.com>
2025-10-11 19:33:29 +0200

    Algorithm Practice

---
+++ leet3069_distribute-elements-into-two-arrays-i.go
@@ -0,0 +1,16 @@
+func resultArray(nums []int) []int {
+    var arr1, arr2 []int
+
+    arr1 = append(arr1, nums[0])
+    arr2 = append(arr2, nums[1])
+
+    for i := 2; i < len(nums); i++ {
+        if arr1[len(arr1)-1] > arr2[len(arr2)-1] {
+            arr1 = append(arr1, nums[i])
+        } else {
+            arr2 = append(arr2, nums[i])
+        }
+    }
+
+    return append(arr1, arr2...)
+}
