b38a4f082f26f99b12393dd9cf610454f4d80455
do-develop <83389587+do-develop@users.noreply.github.com>
2025-12-02 16:08:25 +0100

    Algorithm Practice

---
+++ leet3162_find-the-number-of-good-pairs-i.go
@@ -0,0 +1,11 @@
+func numberOfPairs(nums1 []int, nums2 []int, k int) int {
+    count := 0
+    for _, n1 := range nums1 {
+        for _, n2 := range nums2 {
+            if n1 % (n2 * k) == 0 {
+                count++
+            }
+        }
+    }
+    return count
+}
