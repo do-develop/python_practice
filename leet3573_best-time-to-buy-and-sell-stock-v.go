f3144e32ac044336744e126d2f475e9fe760af8d
do-develop <83389587+do-develop@users.noreply.github.com>
2026-07-01 16:37:18 +0200

    Add files via upload

---
+++ leet3573_best-time-to-buy-and-sell-stock-v.go
@@ -0,0 +1,24 @@
+func maximumProfit(prices []int, k int) int64 {
+    n := len(prices)
+    dp := make([][3]int64, k + 1)
+    // initialize the state on day 0
+    for j := 1; j <= k; j++ {
+        dp[j][1] = int64(-prices[0])
+        dp[j][2] = int64(prices[0])
+    }
+    
+    for i := 1; i < n; i++ {
+        for j := k; j > 0; j-- {
+            // dp[j][0] -> was already flat yesterday, do nothing
+            // dp[j][1] + prices[i] -> was long yesterday, sell today for +prices[i]
+            // dp[j][2] - prices[i] -> was short yesterday, buy back today for -prices[i]
+            dp[j][0] = max(dp[j][0], max(dp[j][1] + int64(prices[i]), dp[j][2] - int64(prices[i])))
+            // was already long yesterday, hold || was flat yesterday, buy open a long today
+            dp[j][1] = max(dp[j][1], dp[j - 1][0] - int64(prices[i])) 
+            // was already short yesterday, hold || was flat yesterday, open a short today
+            dp[j][2] = max(dp[j][2], dp[j - 1][0] + int64(prices[i]))
+        }
+    }
+    
+    return dp[k][0]
+}
