038aba90c7537c168d24e45f7ef66f33f6dbb866
do-develop <83389587+do-develop@users.noreply.github.com>
2026-08-06 08:36:25 +0200

    Add files via upload

---
+++ leet3633_earliest-finish-time-for-land-and-water-rides-i.go
@@ -0,0 +1,33 @@
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+    N := len(landStartTime)
+    M := len(waterStartTime)
+    res := 1000000
+
+    for i := 0; i < N; i++ {
+        for j := 0; j < M; j++ {
+            land := landStartTime[i] + landDuration[i]
+            land_water := land
+
+            if waterStartTime[j] > land_water {
+                land_water = waterStartTime[j]
+            }
+            land_water += waterDuration[j]
+
+            if land_water < res {
+                res = land_water
+            }
+
+            water := waterStartTime[j] + waterDuration[j]
+            water_land := water
+            
+            if landStartTime[i] > water_land {
+                water_land = landStartTime[i]
+            }
+            water_land += landDuration[i]
+            if water_land < res {
+                res = water_land
+            }
+        }
+    }
+    return res
+}
